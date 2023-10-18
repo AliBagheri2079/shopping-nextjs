460d8fd45cbd3725bb4b1be56772eea506b2d89b
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-10-18 19:21:56 +0330

    :sparkles: Feat: create wallpaper, header, main layout and middle layout components

---
+++ src/app/(dashboard)/dashboard/components/main-layout/index.tsx
@@ -0,0 +1,34 @@
+import { Container, Paper } from '@mantine/core';
+import cx from 'clsx';
+import { FC, ReactNode } from 'react';
+
+import classes from './index.module.css';
+import { Dock } from '@/layouts';
+import { SIZE } from '@/lib/utils';
+
+type Props = {
+  children: ReactNode;
+};
+
+const MainLayout: FC<Props> = ({ children }) => (
+  <Container pos='relative' size='xl' h={SIZE.MAX_HEIGHT}>
+    <Paper
+      className={cx(classes.contentPosition, classes.glassmorphism)}
+      mx='md'
+      my={{ base: 'md', md: SIZE.MAIN_LAYOUT_BLOCK_MARGIN }}
+    >
+      {children}
+    </Paper>
+
+    <Paper
+      className={cx(classes.navbarPosition, classes.glassmorphism)}
+      mx='xl'
+      mb='xs'
+      hiddenFrom='md'
+    >
+      <Dock />
+    </Paper>
+  </Container>
+);
+
+export default MainLayout;
