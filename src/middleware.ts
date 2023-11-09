2b040f1d8c8cac1f4528e79e78b93c6bb6ff636c
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-11-09 20:55:07 +0330

    :bug: Bug: fix api route request on middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,11 @@
 import { authMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const regex = new RegExp('/api/*');
+  if (regex.test(request.url)) return NextResponse.next();
+}
 
 export default authMiddleware({
   publicRoutes: ['/'],
