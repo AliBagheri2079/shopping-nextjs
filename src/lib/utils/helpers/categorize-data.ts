4ab7be7dabc0fa350f8548a6077ad50661be0432
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-11-09 20:51:33 +0330

    :sparkles: Feat: create many utility helpers method

--- src/lib/utils/helpers/categorize-data.ts
+++ src/lib/utils/helpers/categorize-data.ts
@@ -1,9 +1,9 @@
-import { CategorizedData, Category } from '@/types';
+import { CategorizeData, Category } from '@/types';
 
 export const categorizeData = <T extends { category: Category }>(
   data: T[],
-): CategorizedData<T> => {
-  const categorizedData: CategorizedData<T> = {};
+): CategorizeData<T> => {
+  const categorizedData: CategorizeData<T> = {};
 
   data.forEach(item => {
     if (!categorizedData[item.category]) categorizedData[item.category] = [];
