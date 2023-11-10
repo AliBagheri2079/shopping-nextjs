a9a017e334992287f35295ba054a60e5b744c674
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-11-10 17:59:28 +0330

    :bug: Bug: fix clerck middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
 import { authMiddleware } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import type { NextFetchEvent, NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const regex = new RegExp('/api/*');
-  if (regex.test(request.url)) return NextResponse.next();
-}
+  if (regex.test(request.url)) {
+    return NextResponse.next();
+  } else {
+    // Create an empty event object if it's not needed for your use case
+    const event = {} as NextFetchEvent;
 
-export default authMiddleware({
-  publicRoutes: ['/'],
-});
+    // Return the result of authMiddleware with both params and event
+    return authMiddleware({
+      publicRoutes: ['/'],
+    })(request, event);
+  }
+}
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
